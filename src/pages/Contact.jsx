025735a39b7928073b0a1e86e17eb927d1c4cc42
sajith <sajith@techis.io>
2023-02-26 16:23:53 +0530

    css remaining

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
-
+import contactLeft from '../assets/contactLeft.webp'
+import '../styles/Contact.css'
 function Contact() {
     return (
-        <div>
-          <h3>This is a contact page</h3>
+        <div className='contact'>
+          <div className='leftSide' style={{ backgroundImage: `url(${contactLeft})`}}></div>
+          <div className='rightSide'></div>
+          <h3 id='cont'>Contact Us</h3>
+          
+          <div id='last'>
+            <form id='contact-form' method='POST'>
+            <label htmlFor='name'>Full Name</label>
+            <input name='name' placeholder='Enter your name' type='text' />
+            <label htmlFor='email'>Email</label>
+            <input name='email' placeholder='Enter your email' type='email' />
+            <label htmlFor='message'>Message</label>
+            <textarea rows='6' placeholder='Enter message' name='message'></textarea>
+            <button type='submit'>Send Message</button>
+            </form>
+          </div>
+          
         </div>
     );
 }
